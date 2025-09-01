dda963278bd38f083c06e5333fbd3e6c150732db
B <sridharb@amd.com>
2025-09-01 20:28:29 +0530

    Calendar Component

---
+++ src/components/CalendarView.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useDispatch, useSelector } from "react-redux";
+import { setSelectedDate } from "../redux/calendarSlice";
+// import BarGraphModal from "./BarGraphModal";
+
+const localizer = momentLocalizer(moment);
+
+const CalendarView = () => {
+  const dispatch = useDispatch();
+  const { selectedDate, data } = useSelector((state) => state.calendar);
+  const [showModal, setShowModal] = useState(false);
+
+  const events = Object.keys(data).map((date) => ({
+    title: `Data available`,
+    start: moment(date, "DD-MM-YYYY").toDate(),
+    end: moment(date, "DD-MM-YYYY").toDate(),
+    allDay: true,
+  }));
+
+  const handleSelectSlot = (slotInfo) => {
+    const dateStr = moment(slotInfo.start).format("DD-MM-YYYY");
+    dispatch(setSelectedDate(dateStr));
+
+    if (!data[dateStr]) {
+      alert("No data found for the selected date.");
+      return;
+    }
+    setShowModal(true);
+  };
+
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        selectable
+        onSelectSlot={handleSelectSlot}
+        onSelectEvent={handleSelectSlot}
+      />
+    </div>
+  );
+};
+
+export default CalendarView;
